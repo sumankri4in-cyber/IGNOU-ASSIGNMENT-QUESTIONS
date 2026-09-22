4e4917268b868f0b61d67a0ecfd67070665000cb
sumankri4in-cyber <sumankri4in@gmail.com>
2026-09-22 07:21:21 +0530

    Create Bihar Sarkari Exam website

---
+++ bihar-sarkari-exam/server.js
@@ -0,0 +1,1 @@
+const express=require('express');const path=require('path');const app=express();const PORT=process.env.PORT||3000;app.use(express.static(path.join(__dirname,'public')));app.get('/health',(req,res)=>res.json({ok:true}));app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'public','index.html')));app.listen(PORT,()=>console.log('Bihar Sarkari Exam running on '+PORT));
